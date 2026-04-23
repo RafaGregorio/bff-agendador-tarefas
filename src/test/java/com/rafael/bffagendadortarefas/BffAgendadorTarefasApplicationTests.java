315ddc835af797ee3dbf22ba7923c0c8b11ec947
Rafael <rafa001.gregorio@gmail.com>
2026-04-23 12:09:23 -0300

    commit inicial

---
+++ src/test/java/com/rafael/bffagendadortarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.bffagendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
