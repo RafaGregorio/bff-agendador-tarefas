5945e71c52b2db612cfe19ac6c453193786de7ed
Rafael <rafa001.gregorio@gmail.com>
2026-05-11 21:29:22 -0300

    adicionado cron

--- src/main/java/com/rafael/bffagendadortarefas/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/rafael/bffagendadortarefas/infrastructure/security/SecurityConfig.java
@@ -6,5 +6,5 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 @SecurityScheme(name = SecurityConfig.SECURITY_SCHEME, type = SecuritySchemeType.HTTP, bearerFormat = "jwt", scheme = "bearer")
 public class SecurityConfig {
 
-    public  static final String SECURITY_SCHEME = "bearerAuth";
+    public static final String SECURITY_SCHEME = "bearerAuth";
 }
